5c15e989f87a0d09160066281b9ec7468425278a
Leandro Silva <leandro.silva@zrp.com.br>
2022-11-22 02:24:08 -0300

    Atualização de script de movimentação
    Mudança para suportar no sistema de inputs da unity

--- LABZRP/Assets/Scripts/Player/PlayerMovement.cs
+++ LABZRP/Assets/Scripts/Player/PlayerMovement.cs
@@ -1,6 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using Vector2 = UnityEngine.Vector2;
+using Vector3 = UnityEngine.Vector3;
 
 //Esse comando faz com que seja necessario o objeto em que o script for aplicado tenha o componente RIGIDBODY
 [RequireComponent(typeof(Rigidbody))]
@@ -10,26 +14,26 @@ public class PlayerMovement : MonoBehaviour
     private Vector3 _inputMovimento;
     //!!! O atributo speed será modificado em breve para comportar modificações por scriptObject
     public int speed = 10;
+
     void Start()
     {
         //_rb está recebendo o componente Rigidbody de onde o script está sendo aplicado
         _rb = GetComponent<Rigidbody>();
     }
 
-    void Update()
+    public void OnMove(InputAction.CallbackContext context)
     {
-        //Estes comandos estão localizados na própria unity Project Settings>InputManager
-        _inputMovimento.x = Input.GetAxisRaw("Horizontal");
-        _inputMovimento.z = Input.GetAxisRaw("Vertical");
+        _inputMovimento = context.ReadValue<Vector2>();
         //O movimento vertical não será utilizado, por isso está sendo zerado
-        _inputMovimento.y = 0;
     }
+
     
     //Para uso de componentes envolvendo fisicas (Nesse caso o RigidBody) é recomendado utilizar o fixed update
     void FixedUpdate()
     {
         //Time.deltaTime normaliza a atualização de comandos independente da quantidade de frames
         _inputMovimento = _inputMovimento.normalized * (speed * Time.deltaTime);
-        _rb.MovePosition(transform.position + _inputMovimento);
+        Vector3 auxVector3 = new Vector3(_inputMovimento.x, 0, _inputMovimento.y);
+        _rb.MovePosition(transform.position + auxVector3);
     }
 }
