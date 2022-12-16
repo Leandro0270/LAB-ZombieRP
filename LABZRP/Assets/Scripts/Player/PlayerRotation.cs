e08dbc3a109b00442738fa73179ede05c23e189c
Leandro Silva <leandro.silva@zrp.com.br>
2022-12-16 16:52:16 -0300

    Implement player status to player rotation

--- LABZRP/Assets/Scripts/Player/PlayerRotation.cs
+++ LABZRP/Assets/Scripts/Player/PlayerRotation.cs
@@ -8,11 +8,14 @@ using UnityEngine;
 using Quaternion = UnityEngine.Quaternion;
 using Vector2 = UnityEngine.Vector2;
 using Vector3 = UnityEngine.Vector3;
+[RequireComponent(typeof(PlayerStats))]
 
 //Esse comando faz com que seja necessario o objeto em que o script for aplicado tenha o componente RIGIDBODY
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerRotation : MonoBehaviour
 {
+
+    private PlayerStats _status;
     private Vector3 _inputRotation;
 
     private Vector3 _inputMouse;
@@ -34,6 +37,7 @@ public class PlayerRotation : MonoBehaviour
     private bool isGamepad;
     void Start()
     {
+        _status = GetComponent<PlayerStats>();
         //_rb está recebendo o componente Rigidbody de onde o script está sendo aplicado
         _rb = GetComponent<Rigidbody>();
     }
@@ -58,18 +62,20 @@ public class PlayerRotation : MonoBehaviour
             _inputRotation = _inputRotation.normalized * Time.deltaTime;
             Quaternion newRotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(_inputRotation), 0.2f);
             if(_lateInputRotation != _inputRotation){
-                _rb.MoveRotation(newRotation);
+                if(!_status.verifyDown() && !_status.verifyDeath())
+                    _rb.MoveRotation(newRotation);
             }
         }else{
             if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out _hit, 100, ground))
-                {
+            {
                     //Nessa variavel está sendo feito a distância do player para onde o raycast está batendo
                     Vector3 playerToMouse = _hit.point - (transform.position);
                     playerToMouse.y = 0;
                     //Nessa variavel está sendo feito o calculo da rotação necessária para o player utilizando o lerp para suavizar
                     Quaternion newRotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(playerToMouse), 0.2f);
-                    _rb.MoveRotation(newRotation);
-                }
+                    if(!_status.verifyDown() && !_status.verifyDeath())
+                        _rb.MoveRotation(newRotation);
+            }
         }
 
     }
