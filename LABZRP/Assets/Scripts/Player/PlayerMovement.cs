43cb5852520178032a37e89b26157ff89b57863c
Leandro Silva <leandro.silva@zrp.com.br>
2022-12-16 16:13:38 -0300

    Add player stats manager to player movement

--- LABZRP/Assets/Scripts/Player/PlayerMovement.cs
+++ LABZRP/Assets/Scripts/Player/PlayerMovement.cs
@@ -8,15 +8,19 @@ using Vector3 = UnityEngine.Vector3;
 
 //Esse comando faz com que seja necessario o objeto em que o script for aplicado tenha o componente RIGIDBODY
 [RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(PlayerStats))]
 public class PlayerMovement : MonoBehaviour
 {
+    private PlayerStats _status;
     private Rigidbody _rb;
     private Vector3 _inputMovimento;
     //!!! O atributo speed será modificado em breve para comportar modificações por scriptObject
-    public int speed = 10;
+    private float _speed;
 
     void Start()
     {
+        _status = GetComponent<PlayerStats>();
+        _speed = _status.getSpeed();
         //_rb está recebendo o componente Rigidbody de onde o script está sendo aplicado
         _rb = GetComponent<Rigidbody>();
     }
@@ -32,8 +36,9 @@ public class PlayerMovement : MonoBehaviour
     void FixedUpdate()
     {
         //Time.deltaTime normaliza a atualização de comandos independente da quantidade de frames
-        _inputMovimento = _inputMovimento.normalized * (speed * Time.deltaTime);
+        _inputMovimento = _inputMovimento.normalized * (_speed * Time.deltaTime);
         Vector3 auxVector3 = new Vector3(_inputMovimento.x, 0, _inputMovimento.y);
+        if(!_status.verifyDown() && !_status.verifyDeath())
         _rb.MovePosition(transform.position + auxVector3);
     }
 }
