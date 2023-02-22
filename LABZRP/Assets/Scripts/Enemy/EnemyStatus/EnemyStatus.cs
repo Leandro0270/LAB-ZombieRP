3c3dbf253e72b63ef15fd96170148a861ad03101
Leandro Silva <leandro.silva@zrp.com.br>
2023-02-22 20:30:29 -0300

    Add blood effect to zombies

--- LABZRP/Assets/Scripts/Enemy/EnemyStatus/EnemyStatus.cs
+++ LABZRP/Assets/Scripts/Enemy/EnemyStatus/EnemyStatus.cs
@@ -6,6 +6,8 @@ public class EnemyStatus : MonoBehaviour
 {
     public ScObEnemyStats _status;
     private GameObject hordeManager;
+    public GameObject blood1;
+    public GameObject blood2;
     private bool isDead = false;
     private float totalLife;
     private float _life;
@@ -30,12 +32,19 @@ public class EnemyStatus : MonoBehaviour
     public void takeDamage(float damage)
     {
         _life -= damage;
+        //instancia o objeto blood1 na posição do inimigo
+        GameObject NewBloodParticle= Instantiate(blood1, new Vector3(transform.position.x, 57, transform.position.z), blood1.transform.rotation);
+        //destroi o objeto blood1 depois de 4 segundos
+        Destroy(NewBloodParticle, 8f);
     }
 
     public void killEnemy()
     {
-        Destroy(GetComponent<BoxCollider>());
+        GetComponent<BoxCollider>().size = new Vector3(1,0.07f,1);
+        GetComponent<BoxCollider>().center = new Vector3(0,-0.5f,0);
         isDead = true;
+        GameObject NewBloodParticle= Instantiate(blood2, new Vector3(transform.position.x, 57, transform.position.z), blood2.transform.rotation);
+        Destroy(NewBloodParticle, 8f);
         hordeManager.GetComponent<HordeManager>().decrementZombiesAlive();
         StartCoroutine(waiter());
 
@@ -50,5 +59,10 @@ public class EnemyStatus : MonoBehaviour
     public float get_life()
     {
         return _life;
+    }
+    
+    public bool isDeadEnemy()
+    {
+        return isDead;
     }
 }
