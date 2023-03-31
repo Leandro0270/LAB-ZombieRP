71af0acb7679d5d0ebedbc44fee8b0013cf77204
Leandro Silva <leandro.silva@zrp.com.br>
2023-03-31 17:55:27 -0300

    Change folder schemes

---
+++ LABZRP/Assets/Scripts/Enemy/ZombieCombat/SpecialZombiesAttacks/SpecialZombiesAttacks.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class SpecialZombiesAttacks : MonoBehaviour
+{
+    public abstract void AtivarPoderEspecial();
+}
+
